47d74ef39a70797a4d803145d3b5848619a95414
Matheus <matheusfelipetrinca@gmail.com>
2025-02-02 00:02:26 -0300

    update-profile-image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,22 @@
 import { LuUser2 } from 'react-icons/lu';
+import { fetchProfileImage } from '@/utils/actions';
+import Image from 'next/image';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage) {
+    return (
+      <Image
+        src={profileImage}
+        width={24}
+        height={24}
+        className="rounded-full object-cover"
+        alt="Profile"
+      />
+    );
+  }
+
   return <LuUser2 className="h-6 w-6 bg-primary rounded-full text-white" />;
 }
 
