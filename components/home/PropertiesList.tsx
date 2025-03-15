22ed50063292db02e13257cea8473f521eed2ca5
Matheus <matheusfelipetrinca@gmail.com>
2025-03-15 02:51:51 -0300

    feat: adding-list-and-card-files

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,4 +1,6 @@
-function PropertiesList() {
+import type { PropertyCardProps } from '@/utils/types';
+
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
   return <div>PropertiesList</div>;
 }
 
