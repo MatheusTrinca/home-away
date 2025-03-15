99dd13bed52fc4d7994a1c01c0ca40feb3fa8645
Matheus <matheusfelipetrinca@gmail.com>
2025-03-15 02:56:10 -0300

    feat: properties-list

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -1,4 +1,6 @@
-function PropertyCard() {
+import { PropertyCardProps } from '@/utils/types';
+
+function PropertyCard({ property }: { property: PropertyCardProps }) {
   return <div>PropertyCard</div>;
 }
 
--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,7 +1,14 @@
 import type { PropertyCardProps } from '@/utils/types';
+import PropertyCard from '../card/PropertyCard';
 
 function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
-  return <div>PropertiesList</div>;
+  return (
+    <section>
+      {properties.map(property => (
+        <PropertyCard key={property.id} property={property} />
+      ))}
+    </section>
+  );
 }
 
 export default PropertiesList;
