63611dc4e518f11ff7154caaece948db8a4e3b7d
Matheus <matheusfelipetrinca@gmail.com>
2025-03-15 17:11:05 -0300

    feat: card-component

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,5 +1,26 @@
-function PropertyRating() {
-  return <div>PropertyRating</div>;
+import { FaStar } from 'react-icons/fa';
+
+function PropertyRating({
+  propertyId,
+  inPage,
+}: {
+  propertyId: string;
+  inPage: boolean;
+}) {
+  // temp
+  const rating = 4.7;
+  const count = 100;
+
+  const className = `flex gap-1 items-center ${inPage ? 'text-md' : 'text-xs'}`;
+  const countText = count > 1 ? 'reviews' : 'review';
+  const contValue = `(${count}) ${inPage ? countText : ''}`;
+
+  return (
+    <span className={className}>
+      <FaStar className="w-3 h-3" />
+      {rating} {contValue}
+    </span>
+  );
 }
 
 export default PropertyRating;
